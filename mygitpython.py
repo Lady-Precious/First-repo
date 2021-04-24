c266feb88c1b8801f65b500605a4b790b8f8e466
Precious Chieke <chiekeprecious24@gmail.com>
2021-04-24 11:01:58 +0100

    my second commit

--- mygitpython.py
+++ mygitpython.py
@@ -1,2 +1,55 @@
 print ('hello world this is me practicing git and github')
 print('yippeee')
+ 
+# TUESDAY 19TH JAN 2021
+# FUNCTIONS
+
+def max(x, y):
+    if x >=y:
+        return x
+    else:
+        return y
+
+print(max(4, 7))
+z  = max (8, 5)
+print (z)
+
+def add(x, y):
+    return x + y
+
+def do_twice(func, x, y):
+    return func(func(x, y), func(x, y))
+
+a = 5
+b = 10
+
+print(do_twice(add, a, b))
+
+#Import modules
+
+import random
+for i in range(5):
+    value = random.randint(1, 6)
+    print(value)
+
+from math import pi
+print(pi)
+
+from math import pi, sqrt
+from math import sqrt as square_root
+print (square_root(100))
+
+def func(x):
+    res = 0
+    for i in range(x):
+        res += i
+    return res
+
+print(func(4)) 
+
+quote = "This python tutorial video is very fast"
+#print("%s %s %s" % ('I like the quiet and serenity', quote))
+
+print("i don't like", end="")
+print(" newlines")
+print ("hi, thanks Jerry for helping me out")
